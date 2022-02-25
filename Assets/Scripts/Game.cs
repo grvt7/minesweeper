04b1c78f77edb4f2e6e50e60da5bc2e3d52362b8
Garvit <r.garvit7@gmail.com>
2022-02-26 00:49:50 +0530

    Code to Generate Numbers

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -26,11 +26,14 @@ public class Game : MonoBehaviour {
     private void NewGame() {
         // Creates the Cell Array
         state = new Cell[width, height];
-        // Populates the cell array with empty tiles
+        // Populates the cell array with Empty Tiles
         GenerateCells();
+        // Populates the grid with Mines
+        GenerateMines();
+        // Generates the numbers for showing how many mines are around the cell
+        GenerateNumbers();
         // Offsets the camera to position around the board
         Camera.main.transform.position = new Vector3(width/2f, height/2f, -10f);
-        GenerateMines();
         // Draws the cell array
         board.Draw(state);
     }
@@ -59,9 +62,61 @@ public class Game : MonoBehaviour {
             if(state[x,y].type == Cell.Type.Mine){
                 i -= 1;
             }
-            
             // Set the type of cell at cell[x,y] as mine
             state[x,y].type = Cell.Type.Mine;
         }
+    }
+
+// Generates the numbers for mines around it
+    private void GenerateNumbers() {
+        // Iterate over the whole grid one by one
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y< height; y++) {
+                // Get the cell state i.e. cell data individually
+                Cell cell = state[x,y];
+                // Check if the current cell is mine then skip it
+                if(cell.type == Cell.Type.Mine){
+                    continue;
+                }
+                // Get the number of mines surrounding the cell
+                cell.number = CountMines(x,y);
+
+                // Checks if there are numbers surrounding the cell
+                if(cell.number > 0) {
+                    cell.type = Cell.Type.Number;
+                }
+                // Set the state of the cell since we made changes to the cell
+                state[x,y] = cell;
+            }
+        }
+    }
+
+    private int CountMines(int cellX, int cellY){
+        // Count to keep track of mines surrounding the tile/cell
+        // X,Y to track the coordinate we are looking at
+        int count = 0, x = 0, y = 0;
+        // Iterate over the 3 adjacent Rows and Columns
+        for(int adjacentX = -1; adjacentX <= 1; adjacentX++){
+            for(int adjacentY = -1; adjacentY <= 1; adjacentY++){
+                // Current position for which this function is called
+                if(adjacentX == 0 && adjacentY == 0){
+                    continue;
+                }
+                // Gets the new positon
+                x = cellX + adjacentX;
+                y = cellY + adjacentY;
+
+                // Check if Coordinate is out of Bounds
+                if(x < 0 || x >= width || y < 0 || y >= height){
+                    continue;
+                }
+                
+                // Checks for Mines in this new position
+                if(state[x,y].type == Cell.Type.Mine) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
